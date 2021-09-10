a97bbc338f33aa2bd9c83fa2358e4edfca6f33c1
Maksym Sagadin <maxsagadin@gmail.com>
2021-09-10 16:24:06 -0700

    initial commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var user1 = {
 var user2 = {
     userName: '@BillGates',
     displayName: 'Bill Gates',
-    joinedDate: 'June 2009',
+    joinedDate: 'July 2009',
     followingCount: 274,
     followerCount: 53800000,
     avatarURL: 'assets/billgates.jpg',
@@ -33,15 +33,18 @@ var user2 = {
     tweets: [
         {
             text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
-            timestamp: '2/10/2021 00:01:20'
+            timestamp: '2/10/2021 00:02:22'
         },
         {
             text: 'Should I start tweeting memes? Let me know in a comment.',
-            timestamp: '2/09/2021 18:37:12'
+            timestamp: '2/09/2021 18:33:14'
         },
         {
             text: 'In 2020, I read a book every hour.',
-            timestamp: '2/09/2021 12:11:51'
+            timestamp: '2/09/2021 12:12:12'
         }
     ]
 };
+
+let header = document.querySelector("header");
+container.
